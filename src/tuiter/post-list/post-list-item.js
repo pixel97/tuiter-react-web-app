8b578532728be0506da8afa062597a38803208ed
Anushka Jain <anushka.dkj@gmail.com>
2022-10-23 22:26:41 -0400

    A6 V6

--- src/tuiter/post-list/post-list-item.js
+++ src/tuiter/post-list/post-list-item.js
@@ -1,5 +1,8 @@
 import React from "react";
 import 'font-awesome/css/font-awesome.min.css';
+import {useRef,useEffect} from 'react';
+
+
 
 const PostListItem = (
 {
@@ -22,9 +25,15 @@ Items = {
    }
 }
 ) => {
+    /*const ref = useRef(null);
+
+    const itemIconNull = ()=>{
+        console.log("Item is null lol!");
+    }*/
+
     return (
                 <li className="list-group-item" style={{backgroundColor:"white"}}>
-                    <div className = "row" style={{height:"35em"}}>
+                    <div className = "row" style={{height:"38em"}}>
                         <div className = "col-1">
                             <img src = {Items.icon} style = {{borderRadius:"50%", height:"50px", width:"50px"}}/>
                         </div>
@@ -34,9 +43,11 @@ Items = {
                             <span style = {{marginBottom:0, color:"gray",fontSize:"12px"}}>@{Items.handle}&nbsp;
                             <span style = {{display:"inline", fontWeight:"normal", fontSize:"12px"}}>-{Items.time}</span>
                             </span></p>
-                            <p>{Items.tuit} <span style = {{color:"blue"}}>{Items.link}</span></p>
 
-                            <img src={Items.image} style = {{height:"60%",width:"100%", borderRadius:"3%"}}/>
+
+                            <p>{Items.tuit} <span style = {{color:"blue"}}>{Items.link}</span></p>
+                           <img src={Items.image} style = {{height:"60%",width:"100%", borderRadius:"3%"}}/>
+
                             <div className = "row justify-content-start">
                                 <div className = "col-3" style = {{color:"gray"}}>
                                     <i className="fa fa-comment"></i>&nbsp;{Items.comment}
@@ -47,7 +58,7 @@ Items = {
                                 <div className = "col-2" style = {{color:"gray"}}>
                                     <i className="fa fa-heart"></i>&nbsp;{Items.likes}
                                 </div>
-                                <div className = "col-2" style = {{color:"gray"}}>
+                                <div className = "col-3" style = {{color:"gray"}}>
                                     <i className="fa fa-upload"></i>
                                 </div>
                             </div>
